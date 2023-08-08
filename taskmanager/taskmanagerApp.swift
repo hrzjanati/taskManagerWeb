2603edaa275a43c24786dd7fd3cadcb05a30dd1e
hossein <hrzjanati@gmail.com>
2023-08-08 11:12:49 +0330

    Initial Commit

---
+++ taskmanager/taskmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerApp.swift
+//  taskmanager
+//
+//  Created by Hossein Janati  on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct taskmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
